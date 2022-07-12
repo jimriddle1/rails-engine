b6291190e7f50d5c85dd36aa11dfaea7d37ffe9c
Jim Riddle <jimriddle0@gmail.com>
2022-07-12 14:30:08 -0500

    included item and merchant items controllers

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::ItemsController < ApplicationController
+
+  def index
+    render json: ItemSerializer.new(Item.all)
+  end
+
+  def show
+    render json: ItemSerializer.new(Item.find(params[:id]))
+  end
+
+  # def show
+  #   render json: MerchantSerializer.new(Merchant.find(params[:id]))
+  # end
+
+end
