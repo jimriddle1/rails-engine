3c5b44b57589d261b61cd452415ef8beb9d2b553
Jim Riddle <jimriddle0@gmail.com>
2022-07-12 13:49:02 -0500

    wrote test for merchant items, have one merchant working

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
     factory :merchant do
       name { Faker::Book.author }
+      # after(:build) do |instance|
+      #   # binding.pry
+      #   3.times { instance.items << FactoryBot.create(:item) }
+      # end
     end
   end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -11,18 +11,55 @@ RSpec.describe 'Merchants API' do
 
     expect(response).to be_successful
 
-    merchants = JSON.parse(response.body, symbolize_names: true)
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    merchants = response_body[:data]
     expect(merchants.count).to eq(3)
     # binding.pry
 
     merchants.each do |merchant|
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
+      expect(merchant[:id]).to be_an(String)
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_an(String)
-
+      expect(merchant).to have_key(:attributes)
+      expect(merchant[:attributes][:name]).to be_an(String)
     end
+  end
+  it 'returns a single merchant' do
+    id = create(:merchant).id
+    create_list(:merchant, 2)
+    get "/api/v1/merchants/#{id}"
+
+    expect(response).to be_successful
+
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    merchant = response_body[:data]
+    # binding.pry
+    expect(merchant.class).to eq(Hash)
+    expect(Merchant.all.count).to eq(3)
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to be_an(String)
+
+    expect(merchant).to have_key(:attributes)
+    expect(merchant[:attributes][:name]).to be_an(String)
+
+    expect(merchant[:attributes][:name]).to_not eq(Merchant.second.name)
+    expect(merchant[:attributes][:name]).to_not eq(Merchant.last.name)
+    # get '/api/v1/merchants'
+  end
+
+  it 'returns a merchants items' do
+    # id = create(:merchant).id
+    # create_list(:item, 4, merchant_id: id)
+
+    id = create(:merchant).id
+    items = create_list(:item, 3, merchant_id: id)
+    # binding.pry
+    get "/api/v1/merchants/#{id}/items"
+
+    expect(response).to be_successful
+
 
 
   end
