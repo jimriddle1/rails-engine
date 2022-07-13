d268ec03948496645b7750262d0ede42a2bb51cc
Jim Riddle <jimriddle0@gmail.com>
2022-07-13 10:34:52 -0500

    wrote out routes for item crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/api/v1/items/:id/merchant', to: 'api/v1/merchant_items#show'
 
   namespace :api do
     namespace :v1 do
-      resources :items, only: [:index, :show]
+      resources :items, only: [:index, :show, :create, :update, :destroy]
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: :merchant_items
       end
