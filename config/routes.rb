32af9df149008212c707e89e68a83910b659cba3
Jim Riddle <jimriddle0@gmail.com>
2022-07-14 15:36:23 -0500

    broke out search methods in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/api/v1/items/:id/merchant', to: 'api/v1/merchant_items#show'
+  #can we use resources to not have to handroll this route?
+  get '/api/v1/merchants/find', to: 'api/v1/merchants/search#find'
+  get '/api/v1/items/find_all', to: 'api/v1/items/search#find_all'
 
   namespace :api do
     namespace :v1 do
