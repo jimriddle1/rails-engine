4951215a1eb85b67ec631848544d207ed98d1ec5
Jim Riddle <jimriddle0@gmail.com>
2022-07-14 15:32:04 -0500

    create invoice and invoice item models

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.search(search_params)
+    where("name ILIKE ?", "%#{search_params}%").first
+  end
+
+  def self.search_all(search_params)
+    where("name ILIKE ?", "%#{search_params}%")
+  end
 end
